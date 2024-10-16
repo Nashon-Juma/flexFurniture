6de512e0a69ca150d3190cce1e3caa2655c7db6f
njuma <Nahshonjumaz@gmail.com>
2024-10-16 18:13:06 +0300

    start from bottom

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+// use App\Models\products;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\products;
+use App\Models\DetailProduct;
+// use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Create 20 categories
+        Category::factory(20)->create();
+
+        // Create 20 products
+        products::factory(20)->create();
+
+        // Create 20 detail products
+        DetailProduct::factory(20)->create();
+
+        // Call UserSeeder to create users
+        UserSeeder::class;
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'username' => 'phong',
+                'email' => 'phong@gmail.com',
+                'phone_number' => 0321565465,
+                'address' => 'TanLap2',
+                'password' => static::$password ??= Hash::make('password'),
+                'role_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'username' => 'OWNER',
+                'email' => 'owner@gmail.com',
+                'phone_number' => 0321565465,
+                'address' => 'TanLap2',
+                'password' => static::$password ??= Hash::make('password'),
+                'role_id' => 0,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
